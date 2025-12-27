870f501aa48148c7b53f3bf2d329293c35845539
Sadiul Hakim <sadiulhakim@gmail.com>
2025-12-27 11:30:51 +0600

    Starting Discussion Board

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Discussion Board</title>
+    <?php require_once('./component/links.php');?>
+</head>
+<body>
+    
+</body>
+</html>
